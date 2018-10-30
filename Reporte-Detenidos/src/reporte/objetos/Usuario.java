c6574a4d021ba7bd5330970b61db0193662fef30
Diana <diana1310fg@gmail.com>
2018-10-30 17:11:53 -0600

    Trabajando en el inicio de la conexión.
    Trabajando en el inicio de la conexión.

---
+++ Reporte-Detenidos/src/reporte/objetos/Usuario.java
@@ -0,0 +1,26 @@
+/*
+ * Modificación: Ing. Flores Garcia Diana Laura.
+ * Fecha: 30/10/18.
+ * Corrigiendo errores y documentando código.
+ */
+
+package reporte.objetos;
+
+public class Usuario {
+	
+	private String usuario;
+	private String passowrd;
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPassowrd() {
+		return passowrd;
+	}
+	public void setPassowrd(String passowrd) {
+		this.passowrd = passowrd;
+	}
+}
